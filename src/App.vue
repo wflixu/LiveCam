aa22a2883ef83c46dab05cf6461e59f9ac753b7c
wflixu <wflixu@88.com>
2024-01-27 22:27:33 +0800

    [ci] 打通用包

--- src/App.vue
+++ src/App.vue
@@ -4,40 +4,37 @@ import { useRouter } from "vue-router";
 import { useSystemStoreHook } from "./store";
 const router = useRouter();
 const systemStore = useSystemStoreHook();
-const checkAvailableDevice = () => {
-  setTimeout(async () => {
-    const deviceInfos = await navigator.mediaDevices.enumerateDevices();
-    console.log(deviceInfos)
-    if (!Array.isArray(deviceInfos)) {
-      router.push("/other/error");
-    }
-    const available = deviceInfos.find((item) => {
-      return item.kind == "videoinput" && item.deviceId;
-    });
-    if (available) {
-      systemStore.setDeviceId(available.deviceId);
-    } else {
-      router.push("/other/error");
-    }
-  }, 500);
+const checkAvailableDevice = async () => {
+
+  const deviceInfos = await navigator.mediaDevices.enumerateDevices();
+  console.log(deviceInfos)
+  if (!Array.isArray(deviceInfos)) {
+    router.push("/other/error");
+  }
+  const available = deviceInfos.find((item) => {
+    return item.kind == "videoinput" && item.deviceId;
+  });
+  if (available) {
+    systemStore.setDeviceId(available.deviceId);
+  } else {
+    router.push("/other/error");
+  }
+
 };
 
-onMounted( () => {
+onMounted(() => {
   checkAvailableDevice();
 });
 </script>
 
 <template>
-    <a-config-provider
-    :theme="{
-      token: {
-        colorPrimary: '#4C8B3B',
-      },
-    }"
-  >
-     <router-view></router-view>
+  <a-config-provider :theme="{
+    token: {
+      colorPrimary: '#4C8B3B',
+    },
+  }">
+    <router-view></router-view>
   </a-config-provider>
- 
 </template>
 
 <style scoped></style>
