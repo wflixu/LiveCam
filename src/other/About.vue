421b30dc8e946799385229bdcdf180a7741d9707
wflixu <wflixu@88.com>
2024-01-27 20:03:32 +0800

    [feat] tranlate content text

--- src/other/About.vue
+++ src/other/About.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="page">
-    <PageTitle title="关于"></PageTitle>
+    <PageTitle title="About"></PageTitle>
     <div class="content">
       <p>
-        icamera ，直播桌面摄像头应用，简单实用，体积小，安装包仅有5 MB。
+        LiveCam is a live desktop camera application. It is not only simple and practical, but also small in size, with an
+        installation package of only 5MB.
       </p>
       <p>
-        如果有问题，请在 <a href="https://github.com/wflixu/icamera/issues">Github</a> 反馈!
+        If you encounter any problems, you can give feedback here <a href="https://github.com/wflixu/icamera/issues">Github</a>, and we will deal with it in time.
       </p>
       <div class="actions">
         <a-button type="primary" @click="onBack">
-          返回
+          Back
         </a-button>
       </div>
     </div>
@@ -20,9 +21,9 @@
 <script setup lang="ts">
 import PageTitle from './components/PageTitle.vue'
 
-import { useRouter} from 'vue-router'
+import { useRouter } from 'vue-router'
 const router = useRouter();
-const onBack = () =>{
+const onBack = () => {
   router.push('/home')
 }
 </script>
